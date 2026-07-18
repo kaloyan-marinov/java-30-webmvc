69f4fc7068649e0b3a9b3a1c9241efd5b7db0f1b
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-07-18 11:14:58 +0200

    add descriptive comments to `src/main/java/com/github/tutorial_about_java_30/TutorialAboutJava30Application.java`

--- src/main/java/com/github/tutorial_about_java_30/TutorialAboutJava30Application.java
+++ src/main/java/com/github/tutorial_about_java_30/TutorialAboutJava30Application.java
@@ -3,6 +3,28 @@ package com.github.tutorial_about_java_30;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+[This] is a convenience annotation that adds all of the following:
+
+- `@SpringBootConfiguration`
+
+  Tags the class as a source of bean definitions for the application context.
+
+- `@EnableAutoConfiguration`
+
+  Tells Spring Boot to start adding beans
+  based on classpath settings, other beans, and various property settings.
+
+  For example, if `spring-webmvc` is on the classpath,
+  this annotation flags the application as a web application
+  and activates key behaviors, such as setting up a `DispatcherServlet`.
+
+- `@ComponentScan`
+
+  Tells Spring to
+  look for other components, configurations, and services in the `com/github` package,
+  letting it find the controllers.
+*/
 @SpringBootApplication
 public class TutorialAboutJava30Application {
 

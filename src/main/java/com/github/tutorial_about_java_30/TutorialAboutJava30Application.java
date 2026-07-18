e757ce8699baa6df2f45900a4dd07941669241ec
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-07-18 09:30:34 +0200

    initialize this as Spring Boot project by (starting from the parent directory and issuing the following commands): ```shell $ curl https://start.spring.io/starter.zip \     -d type=maven-project \     -d language=java \     -d bootVersion=4.1.0 \     -d groupId=com.github \     -d artifactId=tutorial-about-java-30 \     -d packageName=com.github.tutorial-about-java-30 \     -d packaging=jar \     -d javaVersion=25 \     -d dependencies=web \     -o tutorial-about-java-30.zip
    $ unzip tutorial-about-java-30.zip \
        -d tutorial-about-java-30
    
    $ rm tutorial-about-java-30.zip
    ```

---
+++ src/main/java/com/github/tutorial_about_java_30/TutorialAboutJava30Application.java
@@ -0,0 +1,13 @@
+package com.github.tutorial_about_java_30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialAboutJava30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialAboutJava30Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/tutorial_about_java_30/TutorialAboutJava30ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tutorial_about_java_30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialAboutJava30ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
